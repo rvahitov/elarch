84a40952238f5d4057e934bb7f22a7da146e22d9
Рамиль Вахитов <vnrameel@hotmail.com>
2020-02-20 22:46:59 +0300

    fixed warnings

--- src/WebApplication/Controllers/WeatherForecastController.cs
+++ src/WebApplication/Controllers/WeatherForecastController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace WebApplication.Controllers
 {
@@ -15,12 +14,6 @@ namespace WebApplication.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()

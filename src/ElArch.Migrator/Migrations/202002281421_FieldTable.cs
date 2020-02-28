cbe819f233435a58672b3a6b4eda5426f52a542a
Rammil Vakhitov <vnrameel@hotmail.com>
2020-02-28 14:22:42 +0300

    field table migration

---
+++ src/ElArch.Migrator/Migrations/202002281421_FieldTable.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+
+namespace ElArch.Migrator.Migrations
+{
+    [TimestampedMigration(2020, 2, 28, 14, 21)]
+    public sealed class FieldTable : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Field")
+                .WithColumn("FieldId").AsString(128).NotNullable().Indexed()
+                .WithColumn("DocumentTypeId").AsString(256).NotNullable().Indexed()
+                .WithColumn("ValueType").AsString(50).NotNullable().Indexed()
+                .WithColumn("IsRequired").AsBoolean().NotNullable()
+                .WithColumn("IntegerMinValue").AsInt32().Nullable()
+                .WithColumn("IntegerMaxValue").AsInt32().Nullable()
+                .WithColumn("DecimalMinValue").AsDecimal().Nullable()
+                .WithColumn("DecimalMaxValue").AsDecimal().Nullable()
+                .WithColumn("DateTimeMinValue").AsDateTimeOffset().Nullable()
+                .WithColumn("DateTimeMaxValue").AsDateTimeOffset().Nullable()
+                .WithColumn("MinLength").AsInt32().Nullable()
+                .WithColumn("MaxLength").AsInt32().Nullable()
+                .WithColumn("CreationTime").AsDateTimeOffset().NotNullable();
+        }
+    }
+}
